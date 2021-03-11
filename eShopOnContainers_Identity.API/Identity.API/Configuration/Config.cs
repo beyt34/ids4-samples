d17f70822d039e67c1f46be2f4e5d67aa0b140a5
beytullah.taskin <beytullah.taskin@ARSCMTR634>
2021-03-11 10:09:00 +0300

    eShopOnContainers.Identity

---
+++ eShopOnContainers_Identity.API/Identity.API/Configuration/Config.cs
@@ -0,0 +1,76 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Microsoft.eShopOnContainers.Services.Identity.API.Configuration
+{
+    public class Config
+    {
+        // ApiResources define the apis in your system
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                //new ApiResource("orders", "Orders Service"),
+                //new ApiResource("basket", "Basket Service"),
+                //new ApiResource("mobileshoppingagg", "Mobile Shopping Aggregator"),
+                //new ApiResource("webshoppingagg", "Web Shopping Aggregator"),
+                //new ApiResource("orders.signalrhub", "Ordering Signalr Hub"),
+                //new ApiResource("webhooks", "Webhooks registration Service"),
+            };
+        }
+
+        // Identity resources are data like user ID, name, or email address of a user
+        // see: http://docs.identityserver.io/en/release/configuration/resources.html
+        public static IEnumerable<IdentityResource> GetResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        // client want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients(Dictionary<string, string> clientsUrl)
+        {
+            return new List<Client>
+            {
+                // Admin
+                new Client
+                {
+                    ClientId = "admin",
+                    ClientName = "Admin Client",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    // public uri of the client
+                    ClientUri = $"{clientsUrl["Admin"]}",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowAccessTokensViaBrowser = false,
+                    RequireConsent = false,
+                    AllowOfflineAccess = true,
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    RedirectUris = new List<string>
+                    {
+                        $"{clientsUrl["Admin"]}/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        $"{clientsUrl["Admin"]}/signout-callback-oidc"
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "admin"
+                    },
+                    AccessTokenLifetime = 60*60*2, // 2 hours
+                    IdentityTokenLifetime= 60*60*2 // 2 hours
+                }
+            };
+        }
+    }
+}
