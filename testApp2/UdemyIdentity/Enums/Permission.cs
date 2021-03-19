f576cec66a9454a6bea02063b40f473a02b60c9e
beytullah.taskin <beytullah.taskin@ARSCMTR634>
2021-03-19 11:11:53 +0300

    testApp2

---
+++ testApp2/UdemyIdentity/Enums/Permission.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace UdemyIdentity.Enums
+{
+    public enum Permission
+    {
+        [Description("projects.view")]
+        ProjectsViewPolicy,
+
+        [Description("projects.create")]
+        ProjectsCreate,
+
+        [Description("projects.update")]
+        ProjectsUpdate,
+    }
+}
